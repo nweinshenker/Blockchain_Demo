df7a54f5c7f0e9d1902a3bb753a43aa4ac50a9a4
nweinshenker <nweinshenker@wisc.edu>
2019-12-18 14:52:49 -0600

    Restrutured the class of a block

--- blockchain.py
+++ blockchain.py
@@ -1,12 +1,13 @@
 
-class Block:
-
-    def __int__(self, nonce, time_stamp, index, previous_hash, hash):
-        self.nonce = nonce
-        self.time_stamp = time_stamp
-        self.index = index
-        self.previous_hash = previous_hash
-        self.hash = hash
+import hashlib;
+import time;
 
 
-    def calculate_hash(self):
+class Blockchain:
+    def __init__(self):
+        self.current_transactions = []
+        self.chain = []
+        self.nodes = set()
+
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
